6f9139ac1a14c2455c8591547bee5d864f3a29df
felipe2640 <felipe_brito09@hotmail.com>
2022-11-07 21:59:23 -0300

    fix(Dockerfile): bug Dockerfile

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,5 @@
 import { Sequelize } from "sequelize";
-import db from "./config/configdb";
 
-const sequelize = new Sequelize(
-  process.env.DB_HOST ? process.env.DB_HOST : db.host,
-  {}
-);
+const sequelize = new Sequelize(process.env.DB_HOST, {});
 
 export default sequelize;
